dd1acabd1818edaa4a5f5c27dd891654faaf7788
Kent Ross <k@mad.cash>
2024-01-01 19:10:19 -0800

    set traits

--- src/encoding/value_traits.rs
+++ src/encoding/value_traits.rs
@@ -1,7 +1,10 @@
+use alloc::collections::BTreeSet;
 use alloc::vec::Vec;
 use core::iter::Extend;
+#[cfg(feature = "std")]
+use std::collections::HashSet;
 
-/// Trait for containers that store their values in a consistent order.
+/// Trait for containers that store their values in the inserted order, like `Vec`
 pub trait Veclike: Extend<Self::Item> {
     type Item;
     type Iter<'a>: Iterator<Item = &'a Self::Item>
@@ -10,9 +13,32 @@ pub trait Veclike: Extend<Self::Item> {
         Self: 'a;
 
     fn len(&self) -> usize;
+    fn is_empty(&self) -> bool;
     fn iter(&self) -> Self::Iter<'_>;
     fn push(&mut self, item: Self::Item);
+}
+
+/// Trait for set containers.
+pub trait Set: Default {
+    type Item;
+    type Iter<'a>: Iterator<Item = &'a Self::Item>
+    where
+        Self::Item: 'a,
+        Self: 'a;
+
+    fn len(&self) -> usize;
     fn is_empty(&self) -> bool;
+    fn iter(&self) -> Self::Iter<'_>;
+    fn insert(&mut self, item: Self::Item) -> bool;
+}
+
+/// Trait for set containers that store and iterate their items in sorted order.
+pub trait DistinguishedSet: Set + Eq
+where
+    <Self as Set>::Item: Ord,
+    for<'a> <Self as Set>::Iter<'a>: DoubleEndedIterator,
+{
+    fn last(&self) -> Option<&Self::Item>;
 }
 
 impl<T> Veclike for Vec<T> {
@@ -28,6 +54,11 @@ impl<T> Veclike for Vec<T> {
     }
 
     #[inline]
+    fn is_empty(&self) -> bool {
+        Vec::is_empty(self)
+    }
+
+    #[inline]
     fn iter(&self) -> Self::Iter<'_> {
         <[T]>::iter(self)
     }
@@ -36,10 +67,69 @@ impl<T> Veclike for Vec<T> {
     fn push(&mut self, item: T) {
         Vec::push(self, item)
     }
+}
 
-    #[inline]
+impl<T> Set for BTreeSet<T>
+where
+    Self: Default,
+{
+    type Item = T;
+    type Iter<'a> = alloc::collections::btree_set::Iter<'a, T>
+    where
+        Self::Item: 'a,
+        Self: 'a;
+
+    fn len(&self) -> usize {
+        BTreeSet::len(self)
+    }
+
     fn is_empty(&self) -> bool {
-        Vec::is_empty(self)
+        BTreeSet::is_empty(self)
+    }
+
+    fn iter(&self) -> Self::Iter<'_> {
+        BTreeSet::iter(self)
+    }
+
+    fn insert(&mut self, item: Self::Item) -> bool {
+        BTreeSet::insert(self, item)
+    }
+}
+
+impl<T> DistinguishedSet for BTreeSet<T>
+where
+    Self: Set + Eq,
+{
+    fn last(&self) -> Option<&T> {
+        BTreeSet::last(self)
+    }
+}
+
+#[cfg(feature = "std")]
+impl<T> Set for HashSet<T>
+where
+    Self: Default,
+{
+    type Item = T;
+    type Iter<'a> = std::collections::hash_set::Iter<'a, T>
+    where
+        Self::Item: 'a,
+        Self: 'a;
+
+    fn len(&self) -> usize {
+        HashSet::len(self)
+    }
+
+    fn is_empty(&self) -> bool {
+        HashSet::is_empty(self)
+    }
+
+    fn iter(&self) -> Self::Iter<'_> {
+        HashSet::iter(self)
+    }
+
+    fn insert(&mut self, item: Self::Item) -> bool {
+        HashSet::insert(self, item)
     }
 }
 
