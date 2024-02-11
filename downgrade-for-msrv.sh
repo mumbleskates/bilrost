3b2d7538d731e4ac4f3e42568eba96b23f6872e7
Kent Ross <k@mad.cash>
2024-02-10 23:43:38 -0800

    try a new method for explicitly downgrading packages for MSRV support

---
+++ downgrade-for-msrv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script which downgrades dependent packages to match our target MSRV.
+
+set -euxo pipefail
+
+cat msrv-pins.txt | while IFS= read -r LINE; do
+  CRATE="$(echo "${LINE}" | cut --delimiter=' ' --fields=1)"
+  VERSION="$(echo "${LINE}" | cut --delimiter=' ' --fields=2)"
+  cargo update --package "${CRATE}" --precise "${VERSION}"
+done
