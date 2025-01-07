ec76d1b6750a3c45a57fc7c6506753f8005d5f86
Kent Ross <k@mad.cash>
2025-01-07 11:38:01 -0800

    release 0.1011.1

--- bilrost-derive/src/lib.rs
+++ bilrost-derive/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/bilrost-derive/0.1011.0")]
+#![doc(html_root_url = "https://docs.rs/bilrost-derive/0.1011.1")]
 // The `quote!` macro requires deep recursion.
 #![recursion_limit = "4096"]
 #![no_std]
