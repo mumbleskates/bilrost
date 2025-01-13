810f0eb63bf22cf09567fff93bb32c28146a1f2a
Kent Ross <k@mad.cash>
2025-01-13 14:02:01 -0800

    update coverage scripts

--- generate-fuzzing-coverage-report.sh
+++ generate-fuzzing-coverage-report.sh
@@ -6,6 +6,6 @@ cargo fuzz coverage bilrost_fuzz
 
 llvm-cov show --format=html -Xdemangler=rustfilt \
  --instr-profile=fuzz/coverage/bilrost_fuzz/coverage.profdata \
- target/x86_64-unknown-linux-gnu/coverage/x86_64-unknown-linux-gnu/release/bilrost_fuzz \
- src bilrost-types \
- > coverage.html
+ -object target/x86_64-unknown-linux-gnu/coverage/x86_64-unknown-linux-gnu/release/bilrost_fuzz \
+ -sources src bilrost-types \
+ --output-dir=coverage/bilrost_fuzz
--- generate-test-coverage-report.sh
+++ generate-test-coverage-report.sh
@@ -6,17 +6,21 @@ cd $(dirname $0)
 cargo install rustfilt
 
 cargo clean
-rm *.profdata *.profraw || echo ok
 
-RUSTFLAGS="-C instrument-coverage" cargo test --features full-test-suite
-RUSTFLAGS="-C instrument-coverage" cargo test --features test-suite-extra
+mkdir -p coverage/tests/profdata
+rm coverage/tests/profdata/* || echo ok
 
-llvm-profdata merge -sparse *.profraw -o bilrost-tests.profdata
+RUSTFLAGS="-C instrument-coverage" cargo test --workspace --features full-test-suite
+
+mv ./*.profraw coverage/tests/profdata
+llvm-profdata merge -sparse \
+ coverage/tests/profdata/*.profraw \
+ -o coverage/tests/profdata/merged.profdata
 
 llvm-cov show --format=html -Xdemangler=rustfilt \
- --instr-profile=bilrost-tests.profdata \
- --object $(ls target/debug/deps/* | \
+ --instr-profile=coverage/tests/profdata/merged.profdata \
+ -object $(ls target/debug/deps/* | \
     grep -P '^target/debug/deps/(bilrost|derived_message_tests)-[0-9a-f]+$') \
- -sources src \
+ -sources src bilrost-derive/src bilrost-types/src \
  --show-line-counts-or-regions \
- > bilrost-test-coverage.html
+ --output-dir=coverage/tests
