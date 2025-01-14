278511a35fcc70baa555077c195b8ea7f932b30c
Kent Ross <k@mad.cash>
2025-01-14 00:42:13 -0800

    more coverage script fixes

--- generate-test-coverage-report.sh
+++ generate-test-coverage-report.sh
@@ -12,7 +12,7 @@ rm coverage/tests/profdata/* || echo ok
 
 RUSTFLAGS="-C instrument-coverage" cargo test --workspace --features full-test-suite
 
-mv ./*.profraw coverage/tests/profdata
+mv ./*.profraw bilrost-derive/*.profraw bilrost-types/*.profraw coverage/tests/profdata
 llvm-profdata merge -sparse \
  coverage/tests/profdata/*.profraw \
  -o coverage/tests/profdata/merged.profdata
@@ -20,7 +20,7 @@ llvm-profdata merge -sparse \
 llvm-cov show --format=html -Xdemangler=rustfilt \
  --instr-profile=coverage/tests/profdata/merged.profdata \
  -object $(ls target/debug/deps/* | \
-    grep -P '^target/debug/deps/(bilrost|derived_message_tests)-[0-9a-f]+$') \
+    grep -P '^target/debug/deps/(bilrost(_derive|_types)?|derived_message_tests)-[0-9a-f]+$') \
  -sources src bilrost-derive/src bilrost-types/src \
  --show-line-counts-or-regions \
  --output-dir=coverage/tests
