9c67361148ae063665d0f33882eb28bdaa12f3c1
Kent Ross <k@mad.cash>
2025-01-14 03:15:04 -0800

    no ok that's just making it worse i think

--- generate-test-coverage-report.sh
+++ generate-test-coverage-report.sh
@@ -10,9 +10,9 @@ cargo clean
 mkdir -p coverage/tests/profdata
 rm coverage/tests/profdata/* || echo ok
 
-RUSTFLAGS="-C instrument-coverage" cargo test --workspace --features full-test-suite
+RUSTFLAGS="-C instrument-coverage" cargo test --features full-test-suite
 
-mv ./*.profraw bilrost-derive/*.profraw bilrost-types/*.profraw coverage/tests/profdata
+mv ./*.profraw coverage/tests/profdata
 llvm-profdata merge -sparse \
  coverage/tests/profdata/*.profraw \
  -o coverage/tests/profdata/merged.profdata
@@ -20,7 +20,7 @@ llvm-profdata merge -sparse \
 llvm-cov show --format=html -Xdemangler=rustfilt \
  --instr-profile=coverage/tests/profdata/merged.profdata \
  -object $(ls target/debug/deps/* | \
-    grep -P '^target/debug/deps/(bilrost(_derive|_types)?|derived_message_tests)-[0-9a-f]+$') \
- -sources src bilrost-derive/src bilrost-types/src \
+    grep -P '^target/debug/deps/(bilrost|derived_message_tests)-[0-9a-f]+$') \
+ -sources src \
  --show-line-counts-or-regions \
  --output-dir=coverage/tests
