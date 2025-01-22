6de00110d51cca4e543c43093f8ae436641bca80
Kent Ross <k@mad.cash>
2025-01-21 19:06:13 -0800

    old hashbrown support test crate

---
+++ old-hashbrown-check/src/lib.rs
@@ -0,0 +1,26 @@
+use bilrost::Message;
+use hashbrown::{HashMap, HashSet};
+
+#[derive(Debug, Default, PartialEq, Message)]
+struct VeryOldHashbrown {
+    map: HashMap<u64, String>,
+    set: HashSet<i32>,
+}
+
+#[cfg(test)]
+#[test]
+fn it_works() {
+    use bilrost::OwnedMessage;
+
+    let def = VeryOldHashbrown::default();
+    let def_hasher = hashbrown::hash_map::DefaultHashBuilder::default();
+
+    let original = VeryOldHashbrown {
+        map: HashMap::from_iter([(1, "hello".to_owned()), (2, "world".to_owned())]),
+        set: HashSet::from_iter([3, 4]),
+    };
+    let encoded = original.encode_to_vec();
+    let decoded = VeryOldHashbrown::decode(encoded.as_slice()).unwrap();
+
+    assert_eq!(original, decoded);
+}
