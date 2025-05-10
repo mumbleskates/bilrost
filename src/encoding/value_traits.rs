c8dece8bc119eef4513c921c342aecaa0b57a844
Kent Ross <k@mad.cash>
2025-05-10 14:16:18 -0700

    qualify paths in public macro

--- src/encoding/value_traits.rs
+++ src/encoding/value_traits.rs
@@ -75,12 +75,12 @@ macro_rules! empty_state_via_for_overwrite {
         {
             #[inline]
             fn is_empty(&self) -> bool {
-                *self == <Self as EmptyState>::empty()
+                *self == <Self as $crate::encoding::EmptyState>::empty()
             }
 
             #[inline]
             fn clear(&mut self) {
-                *self = <Self as EmptyState>::empty();
+                *self = <Self as $crate::encoding::EmptyState>::empty();
             }
         }
     };
