4b6f5be5265c90a1d398af5fab8802ecd0e141d3
Kent Ross <k@mad.cash>
2025-06-22 01:17:08 -0700

    stop using deprecated criterion black_box whenever possible

--- benches/varint.rs
+++ benches/varint.rs
@@ -1,5 +1,3 @@
-use std::iter;
-
 use bilrost::buf::ReverseBuffer;
 use bilrost::encoding::{
     const_varint, decode_varint, encode_varint, encoded_len_varint, prepend_varint, Capped,
@@ -8,11 +6,17 @@ use bilrost::encoding::{
 use bytes::Buf;
 use criterion::{Criterion, Throughput};
 use rand::{rngs::StdRng, seq::SliceRandom, SeedableRng};
+use std::iter;
+
+#[cfg(not(rustc_1_66))]
+use criterion::black_box;
+#[cfg(rustc_1_66)]
+use std::hint::black_box;
 
 fn benchmark_varint(criterion: &mut Criterion, name: &str, mut values: Vec<u64>) {
     // Shuffle the values in a stable order.
     values.shuffle(&mut StdRng::seed_from_u64(0));
-    let name = format!("varint/{}", name);
+    let name = format!("varint/{name}");
 
     let encoded_len = values
         .iter()
@@ -32,7 +36,7 @@ fn benchmark_varint(criterion: &mut Criterion, name: &str, mut values: Vec<u64>)
                     for &value in &encode_values {
                         encode_varint(value, &mut buf);
                     }
-                    criterion::black_box(&buf);
+                    black_box(&buf);
                 })
             }
         });
@@ -49,7 +53,7 @@ fn benchmark_varint(criterion: &mut Criterion, name: &str, mut values: Vec<u64>)
                     for &value in &encode_values {
                         prepend_varint(value, &mut buf);
                     }
-                    criterion::black_box(&buf);
+                    black_box(&buf);
                 })
             }
         });
@@ -71,7 +75,7 @@ fn benchmark_varint(criterion: &mut Criterion, name: &str, mut values: Vec<u64>)
                     while buf.has_remaining() {
                         let result = decode_varint(&mut buf);
                         debug_assert!(result.is_ok());
-                        criterion::black_box(&result);
+                        black_box(&result);
                     }
                 })
             }
@@ -86,7 +90,7 @@ fn benchmark_varint(criterion: &mut Criterion, name: &str, mut values: Vec<u64>)
                 for &value in &values {
                     sum += encoded_len_varint(value);
                 }
-                criterion::black_box(sum);
+                black_box(sum);
             })
         });
 }
@@ -94,7 +98,7 @@ fn benchmark_varint(criterion: &mut Criterion, name: &str, mut values: Vec<u64>)
 fn benchmark_decode_key(criterion: &mut Criterion, name: &str, mut values: Vec<u64>) {
     // Shuffle the values in a stable order.
     values.shuffle(&mut StdRng::seed_from_u64(0));
-    let name = format!("field/{}", name);
+    let name = format!("field/{name}");
 
     let encoded_len = values
         .iter()
@@ -120,7 +124,7 @@ fn benchmark_decode_key(criterion: &mut Criterion, name: &str, mut values: Vec<u
                     while buf.remaining() > 0 {
                         let result = TagReader::new().decode_key(buf.lend());
                         debug_assert!(result.is_ok());
-                        criterion::black_box(&result);
+                        black_box(&result);
                     }
                 })
             }
