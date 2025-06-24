c5f51f7cf4896938dbf2c50cf3220516c14b9f3d
Kent Ross <k@mad.cash>
2025-06-24 10:50:03 -0700

    add semicolon in cache script for older bash

--- .gitlab-cargo-cache-expire.sh
+++ .gitlab-cargo-cache-expire.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ \
-  !(-f '.cache-info/last-cleaned') || \
+  ! (-f '.cache-info/last-cleaned') || \
   "$(($(date +%s) - $(date +%s -r '.cache-info/last-cleaned')))" -ge "$((7*24*60*60))" \
-]] then
+]] ; then
   echo "cleaning cargo cache..."
   cargo clean
   mkdir -p .cache-info
