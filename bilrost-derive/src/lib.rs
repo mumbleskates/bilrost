cda7f0b86935e218350704e92eaaa86b40fe3d18
Kent Ross <k@mad.cash>
2025-06-28 17:23:34 -0700

    remove message attribute todo

--- bilrost-derive/src/lib.rs
+++ bilrost-derive/src/lib.rs
@@ -1558,13 +1558,6 @@ fn try_oneof(input: TokenStream) -> Result<TokenStream, Error> {
     let crate_ = crate_name();
     let input: DeriveInput = parse2(input)?;
 
-    // TODO: support a "message" word attr that converts any enum variant into an embedded message.
-    //  Unit variants would become the equivalent of an embedded `()` message, tuple variants with
-    //  anonymous fields would work like tuple messages, and braced variants would work just like
-    //  braced messages; the exact same `#[bilrost(..)]` attrs would apply to all the fields inside
-    //  and would encode the variant as if it were a message. This would have to be implemented as a
-    //  special case of the "value" field probably that takes a set of fields that were parsed as if
-    //  it was a (non-oneof) message.
     let PreprocessedOneof {
         ident,
         impl_generics,
