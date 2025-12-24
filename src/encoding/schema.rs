41dd76e168f78e9e0e59038f586209a3e6aa97c3
Kent Ross <k@mad.cash>
2025-12-24 14:14:51 -0800

    more wip schema

--- src/encoding/schema.rs
+++ src/encoding/schema.rs
@@ -1,26 +1,35 @@
+use alloc::borrow::ToOwned;
 use alloc::boxed::Box;
+use alloc::collections::btree_map::Entry;
+use alloc::collections::{BTreeMap, BTreeSet};
 use alloc::fmt::Display;
-use alloc::rc::Rc;
-use core::any::Any;
+use alloc::string::String;
+use core::any::{Any, TypeId};
+use core::cell::RefCell;
+use core::fmt::Formatter;
+use core::marker::PhantomData;
+use alloc::format;
 
 /// Receptacle for a full schema that can record the schemas of many messages.
 pub trait SchemaSet {
     // TODO: notes for a type, like what it should be called etc.
     /// Visits a specific message type. May shortcut if this method has already been invoked
     /// elsewhere for the same type.
-    fn visit_message<M: MessageSchema>(&self);
+    fn visit_message<M: MessageSchema>(&self, name: &str);
 }
 
 /// Receptacle for fields in a single specific message type.
 pub trait FieldSet {
-    // TODO: notes for the whole type, like whether it's a oneof or whatever
+    // TODO: notes for the whole type (distinguished?)
     /// Adds a field to the type.
     fn add_field(&self, name: &str, tag: u32, repr: Box<dyn Display>);
+    /// Registers a oneof on the message by the tags of its mutually exclusive member fields.
+    fn add_oneof(&self, name: &str, tags: &[u32]);
 }
 
 /// A message type that can report the fields in its schema
 pub trait MessageSchema: Any {
-    fn register(schema: Rc<impl SchemaSet + FieldSet>);
+    fn register(schema: impl SchemaSet + FieldSet);
 }
 
 /// Trait for an encoding to describe its representation.
@@ -29,12 +38,127 @@ pub trait ValueSchema<E, T> {
 }
 
 /// Translation standin; proxies Display when the value schema has a description.
-pub struct Repr<E, T>;
+pub struct Repr<E, T>(PhantomData<(E, T)>);
+
+impl<E, T> Repr<E, T> {
+    pub fn new() -> Self {
+        Self(PhantomData)
+    }
+}
 
 impl<E, T> Display for Repr<E, T>
-    where (): ValueSchema<E, T>,
+where
+    (): ValueSchema<E, T>,
 {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
         <() as ValueSchema<E, T>>::repr().fmt(f)
     }
+}
+
+pub struct Schema {
+    types: RefCell<BTreeMap<TypeId, MessageInfo>>,
+}
+
+impl Schema {
+    pub fn new() -> Self {
+        Self{types: Default::default()}
+    }
+}
+
+impl SchemaSet for Schema {
+    fn visit_message<M: MessageSchema>(&self, name: &str) {
+        let mut types = self.types.borrow_mut();
+        match types.entry(TypeId::of::<M>()) {
+            Entry::Vacant(entry) => {
+                let field_set = entry.insert(MessageInfo::new(name));
+                M::register(field_set);
+            }
+            Entry::Occupied(mut entry) => {
+                entry.get_mut().add_name(name);
+                // type was already registered previously
+            }
+        }
+    }
+}
+
+impl Display for Schema {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+
+        for message_idx in 1usize.. {
+
+        }
+        writeln!(f, "{name} {{")?;
+        writeln!(f, "}}")?;
+        Ok(())
+    }
+}
+
+struct MessageInfo {
+    names: BTreeSet<String>,
+    fields: BTreeMap<u32, FieldInfo>,
+    oneofs: BTreeMap<String, BTreeSet<u32>>,
+}
+
+impl MessageInfo {
+    fn new(name: &str) -> Self {
+        Self {
+            names: [name.to_owned()].into(),
+            fields: Default::default(),
+            oneofs: Default::default(),
+        }
+    }
+
+    fn add_name(&mut self, name: &str) {
+        self.names.insert(name.to_owned());
+    }
+
+    fn name(&self) -> String {
+        match self.names.len() {
+            1 => format!("{name:?}", name = self.names.first().unwrap()),
+            _ => format!("(message known as {names:?})", names = self.names),
+        }
+    }
+}
+
+impl FieldSet for &mut MessageInfo {
+    fn add_field(&self, name: &str, tag: u32, repr: Box<dyn Display>) {
+        if self.fields.insert(
+            tag,
+            FieldInfo {
+                name: name.to_owned(),
+                repr,
+            },
+        ).is_some() {
+            panic!(
+                "message {name} registered multiple fields with tag {tag}",
+                name = self.name(),
+            );
+        };
+    }
+
+    fn add_oneof(&self, oneof_name: &str, tags: &[u32]) {
+        let tag_set = tags.into();
+        for (existing_oneof_name, existing_set) in &self.oneofs {
+            if let Some(conflicting_tag) = existing_set.intersection(&tag_set).next() {
+                panic!(
+                    "message {message_name} registered conflicting oneof sets: message declared \
+                    oneofs which both contain tag {conflicting_tag}: {oneof_name:?}={tag_set:?} \
+                    and {existing_oneof_name:?}={existing_set:?}",
+                    message_name = self.name(),
+                );
+            }
+        }
+        if let Some(conflicting_name) = self.oneofs.insert(oneof_name.to_owned(), tag_set) {
+            panic!(
+                "message {message_name} registered multiple oneof sets with the name \
+                {conflicting_name:?}",
+                message_name = self.name(),
+            );
+        }
+    }
+}
+
+struct FieldInfo {
+    name: String,
+    repr: Box<dyn Display>,
 }
