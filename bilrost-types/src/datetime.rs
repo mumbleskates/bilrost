cd2a4b766110939498584b53ff41d4182a40fad0
Kent Ross <k@mad.cash>
2026-07-10 01:24:03 -0700

    gating these tests doesn't make sense, nothing is platform specific here

--- bilrost-types/src/datetime.rs
+++ bilrost-types/src/datetime.rs
@@ -656,15 +656,9 @@ mod tests {
         case("1901-12-13T20:45:52Z", i32::MIN as i64, 0);
         case("1901-12-13T20:45:51Z", i32::MIN as i64 - 1, 0);
 
-        // Skipping these tests on windows as std::time::SystemTime range is low
-        // on Windows compared with that of Unix which can cause the following
-        // high date value tests to panic
-        #[cfg(not(target_os = "windows"))]
-        {
-            case("+292277026596-12-04T15:30:07Z", i64::MAX, 0);
-            case("+292277026596-12-04T15:30:06Z", i64::MAX - 1, 0);
-            case("-292277022657-01-27T08:29:53Z", i64::MIN + 1, 0);
-        }
+        case("+292277026596-12-04T15:30:07Z", i64::MAX, 0);
+        case("+292277026596-12-04T15:30:06Z", i64::MAX - 1, 0);
+        case("-292277022657-01-27T08:29:53Z", i64::MIN + 1, 0);
 
         case("1900-01-01T00:00:00Z", -2_208_988_800, 0);
         case("1899-12-31T23:59:59Z", -2_208_988_801, 0);
