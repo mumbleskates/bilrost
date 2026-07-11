e36a460be8fa35d1d714bdc4357d13415dd31e41
Kent Ross <k@mad.cash>
2026-07-10 17:45:26 -0700

    whitespace nit

--- src/encoding/type_support/hashbrown.rs
+++ src/encoding/type_support/hashbrown.rs
@@ -7,9 +7,11 @@ use crate::DecodeErrorKind;
 use crate::DecodeErrorKind::UnexpectedlyRepeated;
 use core::hash::Hash;
 
-for_overwrite_via_default!(hashbrown::HashSet<T, S>,
-        with generics (T, S),
-        with where clause (T: Eq + Hash, S: Default + core::hash::BuildHasher));
+for_overwrite_via_default!(
+    hashbrown::HashSet<T, S>,
+    with generics (T, S),
+    with where clause (T: Eq + Hash, S: Default + core::hash::BuildHasher)
+);
 
 impl<T, S> EmptyState<(), hashbrown::HashSet<T, S>> for ()
 where
