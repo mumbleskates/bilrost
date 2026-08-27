21fa9f0730ecde4db575a75ddbdfbc5e140684fe
Kent Ross <k@mad.cash>
2026-08-26 18:42:35 -0700

    modernize publish script

--- publish-release.sh
+++ publish-release.sh
@@ -1,29 +1,5 @@
 #!/usr/bin/env bash
-
-# Script which automates publishing a crates.io release of the bilrost crates.
-
 set -ex
+cd "$(dirname "$0")"
 
-if [ "$#" -ne 0 ]
-then
-  echo "Usage: $0"
-  exit 1
-fi
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-CRATES=( \
-  "bilrost-derive" \
-  "." \
-  "bilrost-types" \
-)
-
-for CRATE in "${CRATES[@]}"; do
-  pushd "$DIR/$CRATE"
-
-  echo "Publishing $CRATE"
-
-  cargo publish
-
-  popd
-done
+cargo publish --workspace
